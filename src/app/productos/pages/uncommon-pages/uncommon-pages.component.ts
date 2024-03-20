7bedc5b4118ece04323e1545e31c641ccdd9582b
Gerson Santos <41873993+jherx@users.noreply.github.com>
2024-03-19 18:37:52 -0600

    add: Pipe i18nSelect -  mostrar diferentes textos en función de una variable

--- src/app/productos/pages/uncommon-pages/uncommon-pages.component.ts
+++ src/app/productos/pages/uncommon-pages/uncommon-pages.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-pages',
   templateUrl: './uncommon-pages.component.html',
-  styleUrls: ['./uncommon-pages.component.css']
+  styleUrls: ['./uncommon-pages.component.css'],
 })
 export class UncommonPagesComponent {
+  public name: string = 'gerson';
+  public gender:'male' | 'female' = 'male'
+  public intationMap = {
+    'male':'invitarlo',
+    'female':'invitarla'
+  }
+
+
+  changeClient(){
+    this.name = 'Melissa'
+    this.gender = 'female'
+  }
 
 }
